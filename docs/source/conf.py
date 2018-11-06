ae54744ec9706f30d5ade391113f3d585ddbbcb6
Sam Perriton-Branch <sam.perriton-branch@vualto.com>
2018-11-06 14:29:23 +0000

    Updated theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,11 @@ project = u'vualto-task-engine-documentation'
 copyright = u'2018, VUALTO'
 author = u'VUALTO'
 
-html_context = {'home_link': 'https://vualto-public-documentation.readthedocs-hosted.com'}
+html_context = {
+    'home_link': 'https://vualto-public-documentation.readthedocs-hosted.com',
+    'product_logo': '',
+    'base_breadcrumb_label': 'Task-Engine'
+}
 
 # The short X.Y version
 version = u''
